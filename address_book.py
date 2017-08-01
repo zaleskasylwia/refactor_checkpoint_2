1fc73f16b77576d2bd80f14dd379d611361d4562
sylwia <zaleska.syl@gmail.com>
2017-08-01 17:12:35 +0200

    Add method to add address to address book, raise TypeError

--- address_book.py
+++ address_book.py
@@ -5,4 +5,10 @@ from work_address import WorkAddress
 class AddressBook:
     def __init__(self, name):
         self.name = name
-        self.address = []
+        self.addresses = []
+
+    def add_address(self, address):
+        if isinstance(address, Address):
+            return self.addresses.append(address)
+        else:
+            raise TypeError
