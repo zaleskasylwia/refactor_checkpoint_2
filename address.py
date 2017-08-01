df2aa15a66a79a72f2d65de1ae1087d5bb200723
sylwia <zaleska.syl@gmail.com>
2017-08-01 16:27:41 +0200

    Add class Addres with init method

---
+++ address.py
@@ -0,0 +1,13 @@
+class Address:
+    def __init__(self, person, city, street, house_no):
+        self.person = person
+        self.city = city
+        self.street = street
+        self.house_no = house_no
+
+    def get_full_address(self):
+        return "{}, {}, {} {}".format(self.person, self.city, self.street, self.house_no)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+        
