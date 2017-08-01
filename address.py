e67771f54b4deb90240e6ec9bbfdd2b668e5eff2
sylwia <zaleska.syl@gmail.com>
2017-08-01 16:30:06 +0200

    Add method to compare two objects

--- address.py
+++ address.py
@@ -7,3 +7,6 @@ class Address:
 
     def get_full_address(self):
         return "{}, {}, {} {}".format(self.person, self.city, self.street, self.house_no)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
