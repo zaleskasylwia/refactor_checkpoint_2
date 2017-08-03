bd7171e969dc55a6bb111af3dd0760835e91995c
sylwia <zaleska.syl@gmail.com>
2017-08-03 16:39:44 +0200

    Fix create and save method, import csv module

--- address_book.py
+++ address_book.py
@@ -27,54 +27,34 @@ class AddressBook:
 
         while not sorted:
             sorted = True
-            for address in range(length):
-                if self.addresses[address].get_full_address() > self.addresses[address + 1].get_full_address():
+            for index in range(length):
+                if self.addresses[index] > self.addresses[index + 1]:
                     sorted = False
-                    self.addresses[address], self.addresses[address + 1] = self.addresses[address + 1], self.addresses[address]
+                    self.addresses[index], self.addresses[index + 1] = self.addresses[index + 1], self.addresses[index]
 
         return (self.addresses)
 
     @classmethod
     def create_from_csv(cls, list_name, csv_path):
         address_book = cls(list_name)
-
-        with open(csv_path, 'r') as address_data:
-            next(address_data)
-            lines = address_data.readlines()
-        table_with_addresses = [element.strip().split("\n") for element in lines]
-
-        for line in table_with_addresses:
-            if len(line) == 4:
-                address = Address(line[0], line[1], line[2], line[3])
-                address_book.add_address(address)
-
-            elif len(line) == 5:
-                address = WorkAddress(line[0], line[1], line[2], line[3], line[4])
-                address_book.add_address(address)
-
+        with open(csv_path) as csvfile:
+            address_reader = csv.reader(csvfile)
+            next(address_reader)  # skip heades
+            for data in address_reader:
+                company = data[4]
+                if company:
+                    address_book.add_address(WorkAddress(data[0], data[1], data[2], data[3], data[4]))
+                else:
+                    address_book.add_address(Address(data[0], data[1], data[2], data[3]))
         return address_book
 
     def save_to_csv(self):
-        HEADERS = 'person,city,street,house_no,company'
-        list_to_save = []
-        for address in self.addresses:
-            if isinstance(address, WorkAddress):
-                work_address_list = [WorkAddress.person, WorkAddress.city, WorkAddress.street, WorkAddress.house_no, WorkAddress.company]
-                list_to_save.append(work_address_list)
-            else:
-                address_list = [Address.person, Address.city, Address.street, Address.house_no]
-                list_to_save.append(address_list)
-        string_to_save = "\n".join(list_to_save)
-
-        with open("{}.csv".format(self.name), 'w') as csvfile:
-            csvfile.write(HEADERS)
-            csvfile.write(string_to_save)
-
-'''        with open("{}.csv".format(self.name), 'w') as csvfile:
-            csvfile.write(HEADERS)
-
+        HEADERS = ['person', 'city', 'street', 'house_no', 'company']
+        with open('{}.csv'.format(self.name), 'w', newline='') as csvfile:
+            addresswriter = csv.writer(csvfile)
+            addresswriter.writerow(HEADERS)
             for a in self.addresses:
                 if isinstance(a, WorkAddress):
-                    csvfile.write(a.person, a.city, a.street, a.house_no, a.company)
+                    addresswriter.writerow([a.person, a.city, a.street, a.house_no, a.company])
                 else:
-                    csvfile.write(a.person, a.city, a.street, a.house_no)'''
+                    addresswriter.writerow([a.person, a.city, a.street, a.house_no, ''])
